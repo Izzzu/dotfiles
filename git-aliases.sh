71a5b23087baf60a7f66f5e566208f601cf25618
Izabela <ikulakowska@microsoft.com>
2020-10-19 12:28:35 +0200

    Update git-aliases.sh

--- git-aliases.sh
+++ git-aliases.sh
@@ -3,6 +3,7 @@ if [[ $(which git > /dev/null; echo $?) == "0" ]]; then
     git config --global --replace-all alias.st 'status'
     git config --global --replace-all alias.lt 'log --graph --oneline --decorate --all'
     git config --global --replace-all alias.cb "checkout -b"
+    git config --global --replace-all alias.last "log -1 HEAD"
 else
     echo "git not found - skipping aliases"
 fi
